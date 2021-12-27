58bda549f61be1ba1a392a61851fccf4c0ebbc80
Darragh Sherwin <dsherwin@overstock.com>
2021-12-27 18:00:48 +0000

    Script for getting CSVs

---
+++ data/get_csvs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-casualty-2020.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-vehicle-2020.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-accident-2020.csv
+
+
+
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-casualty-2019.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-vehicle-2019.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-accident-2019.csv
+
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-casualty-2018.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-vehicle-2018.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-accident-2018.csv
+
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-casualty-2017.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-vehicle-2017.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-accident-2017.csv
+
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-casualty-2016.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-vehicle-2016.csv
+/usr/bin/wget https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-accident-2016.csv
