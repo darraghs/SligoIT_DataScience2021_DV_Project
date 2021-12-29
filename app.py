e4eb12538484182a45aecbdad303b9b0e55dae50
Darragh Sherwin <dsherwin@overstock.com>
2021-12-29 17:37:27 +0000

    More filtering

--- app.py
+++ app.py
@@ -67,21 +67,14 @@ bootstrap_rows = html.Div(
                             html.Div([
                                 dbc.Label("Local Authority"),
 
-                                dcc.Checklist(id='local_auth_list',
-                                              options=[{'label':str(b[1]),'value':b[0]} for b in accident_data_lookup.accident_data_lookup['local_authority_district'].items],
-                                              value=[],
-                                              ),
-
                                 dcc.Dropdown(id="seelct_local_authority",
-                                             options=[
-                                                 {"label": "2016", "value": 2016},
-                                                 {"label": "2017", "value": 2017},
-                                                 {"label": "2018", "value": 2018},
-                                                 {"label": "2019", "value": 2019},
-                                                 {"label": "2020", "value": 2020}],
-                                             multi=False,
-                                             value=2020
-                                             )
+                                             options=[{'label': str(b[1]), 'value': b[0]} for b in
+                                                      accident_data_lookup.accident_data_lookup[
+                                                          'local_authority_district'].items],
+                                             multi=True,
+                                             value=[],
+                                             ),
+
                             ])
                         )
                     )
@@ -140,12 +133,12 @@ app.layout = dbc.Container(
     [Output('crash_table', 'data'),
      Output(component_id='output_container', component_property='children'),
      Output(component_id='crash_map', component_property='figure')],
-    [   Input('severity-input', 'value'),
-        Input('crash_map', 'clickData'),
+    [Input('severity-input', 'value'),
+     Input('crash_map', 'clickData'),
      Input(component_id='slct_year', component_property='value')]
 )
 def update_map(severity, marker_selection, year_selected):
-    if year_selected is not None and len(severity)>0:
+    if year_selected is not None and len(severity) > 0:
         global accident_df
 
         triggered_id = callback_context.triggered[0]['prop_id']
@@ -168,6 +161,7 @@ def apply_map_fitlers(severities, year):
     fig = utils.getmapfigure(accident_df)
     return fig
 
+
 def update_accident_table(selection):
     if selection is not None:
         global accident_df
@@ -180,7 +174,7 @@ def update_accident_table(selection):
         for i in accident_data:
             if i in accident_data_lookup.accident_data_lookup.keys():
                 lookup = accident_data_lookup.accident_data_lookup[i]
-                if len(accident_data[i].values) > 0 :
+                if len(accident_data[i].values) > 0:
                     value = accident_data[i].values[0]
                     if isinstance(value, numpy.int64):
                         value = value.item()
