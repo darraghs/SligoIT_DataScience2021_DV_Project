cc3106ff77dc73c34ec552ac6dbe34027f4421f5
Darragh Sherwin <dsherwin@overstock.com>
2021-12-29 10:23:12 +0000

    Show map initially

--- accidentdashboard/utils.py
+++ accidentdashboard/utils.py
@@ -29,18 +29,19 @@ def getaccidentdf(year):
     return cleanDF(accident_df)
 
 def getmapfigure(accident_df):
-    token = open(".mapbox_token").read()
-    accident_df['accident_severity'] = accident_df['accident_severity'].astype(str)
-    fig = px.scatter_mapbox(accident_df, lat="latitude", lon="longitude", hover_name="accident_severity",
-                            hover_data=["speed_limit", "number_of_vehicles"],
-                            custom_data=['accident_index'],
-                            color="accident_severity",
-                            color_discrete_sequence=crash_colours,
-                            zoom=4, height=800, width=600)
+    if accident_df is not None:
+        token = open(".mapbox_token").read()
+        accident_df['accident_severity'] = accident_df['accident_severity'].astype(str)
+        fig = px.scatter_mapbox(accident_df, lat="latitude", lon="longitude", hover_name="accident_severity",
+                                hover_data=["speed_limit", "number_of_vehicles"],
+                                custom_data=['accident_index'],
+                                color="accident_severity",
+                                color_discrete_sequence=crash_colours,
+                                zoom=4, height=800, width=600)
 
 
-    fig.update_layout(mapbox_style="open-street-map", mapbox_accesstoken=token)
-    fig.update_mapboxes(center_lat=55, center_lon=-3.5)
-    fig.update_layout(margin={"r":1,"t":1,"l":1,"b":1})
-    fig.update_layout(height=800)
-    return fig
+        fig.update_layout(mapbox_style="open-street-map", mapbox_accesstoken=token)
+        fig.update_mapboxes(center_lat=55, center_lon=-3.5)
+        fig.update_layout(margin={"r":1,"t":1,"l":1,"b":1})
+        fig.update_layout(height=800)
+        return fig
