a3c34a4f8a3802c4a402002a3304e9f25acee98a
Darragh Sherwin <dsherwin@overstock.com>
2022-01-04 23:31:31 +0000

    Clean up

--- app.py
+++ app.py
@@ -107,9 +107,10 @@ def apply_map_fitlers(year, severities, local_auth_selected, lat_min, lat_max, l
         filter_accident_df = filter_accident_df[
             filter_accident_df['local_authority_district'].isin(local_auth_selected)].copy()
 
-    filter_accident_df = filter_accident_df[
-        filter_accident_df['latitude'].between(lat_min, lat_max) & filter_accident_df['longitude'].between(lon_min,
-                                                                                                           lon_max)].copy()
+    if lat_max is not None && lat_min is not None and lon_min is not None and lon_max is not None:
+        filter_accident_df = filter_accident_df[
+            filter_accident_df['latitude'].between(lat_min, lat_max) & filter_accident_df['longitude'].between(lon_min,
+                                                                                                               lon_max)].copy()
     print(f'Shape after filering: {filter_accident_df.shape}')
 
     return filter_accident_df
