a5052cbe63a1d1397e4c043da91193bd74dfc708
Darragh Sherwin <dsherwin@overstock.com>
2022-01-04 17:28:29 +0000

    Graphs....

--- accidentdashboard/layout/app_tabs.py
+++ accidentdashboard/layout/app_tabs.py
@@ -22,7 +22,7 @@ def get_html_tabs(accident_df):
                              multi=False,
                              value=[],
                              ),
-                dcc.Graph(id='graph_viz')
+                dcc.Graph(id='graph_viz', figure={})
 
             ], style={'min-height': '750px'}
         ),
--- accidentdashboard/utils.py
+++ accidentdashboard/utils.py
@@ -144,7 +144,7 @@ def camel_case(s):
 def get_graph_fig(accident_stats_df, x_axis, key):
 
     print(f' X-Axis: {x_axis} {type(x_axis)}, key: {key}, {type(key)}')
-    if type(x_axis) == 'str' and type(key) == 'str':
+    if isinstance(x_axis, str) and isinstance(key, str):
 
         graph_df = accident_stats_df[[x_axis, key]].sort_values(by=[x_axis, key])
 
