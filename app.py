45aa53ba5b288674fd66657766106f0a7fc2b57d
Darragh Sherwin <dsherwin@overstock.com>
2022-01-04 22:20:57 +0000

    Graphs....

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def apply_map_fitlers(year, severities, local_auth_selected, lat_min, lat_max, l
     print(f'Shape before filtering: {filter_accident_df.shape}')
     if severities is not None and len(severities) > 0:
         filter_accident_df = filter_accident_df[filter_accident_df['accident_severity'].isin(severities)].copy()
-    print(f'Shape after severity: {filter_accident_df.shape}')
+        print(f'Shape after severity: {filter_accident_df.shape}')
     if len(local_auth_selected) > 0:
         filter_accident_df = filter_accident_df[filter_accident_df['local_authority_district'].isin(local_auth_selected)].copy()
         print(f'Shape after local auth: {filter_accident_df.shape}')
